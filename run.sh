399b83089cc287a24cce7430f5938b1c52941ade
tig <tig@example.com>
2017-02-11 16:52:55 +0900

    gcc -> g++

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 # GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 # gcc -fdiagnostics-color=auto -o app main.cpp &&\
-gcc -fdiagnostics-color=auto -o app main.cpp &&\
+g++ -o app main.cpp &&\
 ./app
